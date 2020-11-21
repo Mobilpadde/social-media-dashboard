f6717289aa9ed54087d5d39827e95fc182137f71
Mads Bram Cordes <Mobilpadde@gmail.com>
2020-11-21 03:50:23 +0100

    Remove /docs

--- docs/_dist_/components/Toggle.css.proxy.js
+++
@@ -1,5 +0,0 @@
-if(typeof document!="undefined"){const t=`.mode.svelte-1fm7uqp{display:flex;justify-items:baseline;margin:1.25rem}label.svelte-1fm7uqp{font-size:0.875rem;color:var(--desaturated-text);font-weight:700}.toggle.svelte-1fm7uqp{background-image:linear-gradient(
-      to right,
-      var(--toggle-bg-1),
-      var(--toggle-bg-2)
-    );position:relative;width:2.5rem;height:1.25rem;margin:0 0.25rem;padding:0.5rem;border-radius:9999px;appearance:none}.toggle.svelte-1fm7uqp::after{content:"";background-color:hsl(230, 17%, 14%);width:1rem;height:1rem;display:block;position:absolute;top:0.125rem;left:0.125rem;border-radius:50%;transition:cubic-bezier(0.175, 0.885, 0.32, 1.275) left 250ms}.toggle.svelte-1fm7uqp:checked{--toggle-bg-1:hsl(210, 78%, 56%);--toggle-bg-2:hsl(146, 68%, 55%)}.toggle.svelte-1fm7uqp:checked::after{left:1.375rem}`,e=document.createElement("style"),o=document.createTextNode(t);e.type="text/css",e.appendChild(o),document.head.appendChild(e)}
